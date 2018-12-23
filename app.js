abdd22cdabc8b8288c027c2d05b37b3cc05c58c8
Samuel Pinheiro <pinheirolaoluwa@gmail.com>
2018-12-23 23:37:08 +0100

    update

--- app.js
+++ app.js
@@ -21,31 +21,54 @@ app.post('*',(req,res) => {
     if( text === ""){
         let response = `
         CON Welcome to my simple USSD app. Select optionns from below
-        1. My Account
-        2. My Number
+        1. Take a loan
+        2. Loan Balance
+        3. Check Eligility
         `;
-    res.send(response)
+    res.send(response);
+
     } else if( text === '1') {
         // buz logic for first level
        let response = `
        CON CHoose from the options below
-       1. Account balance
-       2. Account Number
+       1. NGN 1000
+       2. NGN 3000
+       3. NGN 5000
+       4. NGN 7000
        `;
-
        res.send(response);
+
     } else if(text === '2'){
-        let phone = '09032487893';
-        let response = `END Your phone number is ${phone}`
+        let balance = '5000';
+        let response = `END Your loan balance is -${balance}
+        Please pay up in time.
+        `
         res.send(response);
+
+    } else if(text === '3'){
+        let response = `END You are currently eligible for loan`
+        res.send(response);
+
     } else if(text === '1*1'){
-        let balance = 10000;
-        let response = `END Your accout balance is NGN ${Math.floor(balance)}`
+        let balance = 1000;
+        let response = `END Your accout balance has been credited with NGN ${balance}`
         res.send(response);
+
     } else if(text === '1*2'){
-        let account = 1289228390;
-        let response = `END Your accout balance is ${account}`
+        let balance = 3000;
+        let response = `END Your accout balance has been credited with NGN ${balance}`
         res.send(response);
+    
+    } else if(text === '1*3'){
+        let balance = 5000;
+        let response = `END Your accout balance has been credited with NGN ${balance}`
+        res.send(response);
+    
+    } else if(text === '1*4'){
+        let balance = 4000;
+        let response = `END Your accout balance has been credited with NGN ${balance}`
+        res.send(response);
+
     } else {
         res.status(400).send('Bad request');
     }
